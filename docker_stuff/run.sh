b51b8c319a83cbb14c84b9f181c63434dd8b9ac1
KavyaVarma <kavya.varma98@gmail.com>
2019-09-27 22:19:19 +0530

    added running from docker

--- docker_stuff/run.sh
+++ docker_stuff/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 : '
 	0: successful
@@ -7,7 +7,7 @@
 	3: test case error -> /logs/test_cases_timeout
 '
 
-path='../information/';
+path='/information';
 problem_id=$1;
 language=$2;
 n_test_cases=$(ls $path/$problem_id/test_cases/ip* | wc | awk '{print $1}');
@@ -87,16 +87,15 @@ if [[ $language == "python" ]]
 then
 	for i in $(seq 1 $n_test_cases);
 	do
-		timeout 1 python $path/$problem_id/codes/code.py < $path/$problem_id/test_cases/ip$i > $path/$problem_id/test_cases/currop 2> $path/$problem_id/logs/compilation_error
+		timeout 1 python3 $path/$problem_id/codes/code.py < $path/$problem_id/test_cases/ip$i > $path/$problem_id/test_cases/currop 2> $path/$problem_id/logs/compilation_error
 		
 		error=$?;
-		
+
 		if [[ $error -eq 1 ]]
 		then
 			echo 1;
 			exit;
-
-		elif [[ $error -eq 124 ]]
+		elif [[ $error -ne 0 ]]
 		then
 			echo $i > $path/$problem_id/logs/test_cases_timeout;
 			echo 3;
