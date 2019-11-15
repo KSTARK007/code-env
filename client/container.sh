1b8b614de68a95c3208ce2adfc8719900b12afa5
Adarsh64 <adarshnayak64@gmail.com>
2019-11-15 22:50:38 +0530

    added extension

--- client/container.sh
+++ client/container.sh
@@ -10,11 +10,11 @@ problem_id=$1;
 language=$2;
 timelimit=$3;
 usn=$4;
+extension=$5;
+docker run -v ~/eclipse-workspace/codeEnv/information:/information -p 8080:8080 coder ./run.sh $problem_id $language $timelimit
 
-docker run -v ~/eclipse-workspace/client/information:/information -p 8080:8080 coder ./run.sh $problem_id $language $timelimit
-
-codelocation='code=@./information/'$problem_id'/codes/code'
-output=$(cat ./information/$problem_id/logs/test_cases_output | tr "\n" "_")
+codelocation='code=@/home/adarsh/eclipse-workspace/codeEnv/information/'$problem_id'/codes/code'$extension
+output=$(cat ~/eclipse-workspace/codeEnv/information/$problem_id/logs/test_cases_output | tr "\n" "_")
 status='status='$output
 
 curl -w "%{http_code}\n" -X POST -F "language="$language \
